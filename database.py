1e83b080286b505dc3eac14b345f80949e986067
Bobby <bobbybasy@gmail.com>
2022-02-06 23:30:50 -0600

    Create SQLite3 DB Class with functionality

---
+++ database.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+class DB:
+    def __init__(self):
+        self.con = sqlite3.connect('books.db')
+        self.cur = self.con.cursor()
+        self.cur.execute(
+                "CREATE TABLE IF NOT EXISTS Books ( \
+                    isbn INTEGER PRIMARY KEY, \
+                    title TEXT, \
+                    author TEXT, \
+                    pagesread INTEGER, \
+                    totalpages INTEGER, \
+                    averagerating INTEGER \
+                    )"
+                )
+        self.conn.commit()
+
+    def __del__(self):
+        self.conn.commit()
+        self.conn.close()
+
+    def view(self):
+        self.curr.execute("SELECT * FROM Books")
+        row = self.cur.fetchall()
+        return row
+
+    def insert(self, isbn):
+        self.cur.execute(
+                "INSERT INTO Books VALUES (?)", (isbn))
+        self.conn.commit()
+        self.view()
+
+    def update(self, ISBN, title, author, pagesread):
+        self.cur.execute(
+            "UPDATE Books SET title=?, author=?, pagesread=? WHERE id=?", 
+                (title, author, pagesread, isbn))
+
+    def search(self, title="", author=""):
+        self.cur.execute(
+            "SELECT * FROM Books Where title=? OR author=?", (title, author))
+        row = self.cur.fetchall()
+        return row
+
+
+
