60a05d765f754f04c1638bdb31340d4680f78021
Bobby <bobbybasy@gmail.com>
2022-02-07 18:50:12 -0600

    Query function to search Google Books API

---
+++ Model/googleBooksAPI.py
@@ -0,0 +1,13 @@
+import requests
+import config
+
+class booksAPI():
+    def search(self, query):
+        params = {"q":query, 'key':config.apikey}
+        url = "https://www.googleapis.com/books/v1/volumes"
+        response = requests.get(url, params=params)
+        data = response.json()
+        print(data["items"][0]["volumeInfo"]["title"])
+
+bk = booksAPI()
+bk.search("9780679721109")
